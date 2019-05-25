18899a420456ff1d500986ed541c27ecfd26ba58
Konstantin V <mail@k-vinogradov.ru>
2019-05-25 14:34:37 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/application.css';
+
+// import faker from 'faker';
+// import gon from 'gon';
+// import cookies from 'js-cookie';
+// import io from 'socket.io-client';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+document.getElementById('chat').textContent = "It's a chat element"
